d7451cc0a5aab1c4f8d04d52cbd9dd665f07aa5d
spindling <jeremy.sean009@gmail.com>
2026-02-26 13:18:52 -0500

    Created main_page.mustache view file and dbinit.js with sample data

---
+++ dbinit.js
@@ -0,0 +1,20 @@
+const sqlite3 = require("sqlite3");
+const sqlite = require("sqlite");
+
+async function dbinit()
+{
+  const db = await sqlite.open({
+    filename: "notes.db",
+    driver: sqlite3.Database
+  });
+
+  await db.exec("DROP TABLE IF EXISTS Notes");
+  await db.exec("CREATE TABLE Notes (title TEXT, content TEXT, star INTEGER, image BLOB, timestamp TEXT, charcount INTEGER)");
+
+  await db.run("INSERT INTO Employees VALUES (?,?,?,?,?,?)", ['Plants', 'Plants are nice!', '1', 'null', '2024-02-26', '12141' ] );
+  await db.run("INSERT INTO Employees VALUES (?,?,?,?,?,?)", ['Animals', 'Animals are nice!', '0', 'null', '2024-02-23', '2424' ] );
+  await db.run("INSERT INTO Employees VALUES (?,?,?,?,?,?)", ['Mushrooms', 'Mushrooms are not nice!', '1', 'null', '2024-02-21', '15351' ] );
+
+}
+
+dbinit();
