823f68c5dd2dfc17052e0d24d27114f3133ab016
spindling <jeremy.sean009@gmail.com>
2026-02-26 13:31:12 -0500

    Implement database connection and  getAllNotes function

---
+++ app.model.js
@@ -0,0 +1,19 @@
+const sqlite3 = require("sqlite3").verbose();
+const sqlite = require("sqlite");
+
+let db; 
+
+async function makeConnection()
+{
+    db = await sqlite.open({
+        filename: "notes.db",
+        driver: sqlite3.Database
+    });
+}
+
+async function getAllNotes()
+{
+    const results = await db.all("SELECT rowid, * FROM Notes");
+    return results;
+}
+module.exports = { makeConnection };
