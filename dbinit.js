3371041a16af153a78760882d70cd8b807d77063
spindling <jeremy.sean009@gmail.com>
2026-02-26 22:33:12 -0500

    Modified dbinit values

--- dbinit.js
+++ dbinit.js
@@ -11,9 +11,9 @@ async function dbinit()
   await db.exec("DROP TABLE IF EXISTS Notes");
   await db.exec("CREATE TABLE Notes (title TEXT, content TEXT, starred INTEGER, image BLOB, timestamp TEXT, charcount INTEGER)");
 
-  await db.run("INSERT INTO Notes VALUES (?,?,?,?,?,?)", ['Plants', 'Plants are nice!', '1', '', '2024-02-26', '12141' ] );
-  await db.run("INSERT INTO Notes VALUES (?,?,?,?,?,?)", ['Animals', 'Animals are nice!', '0', '', '2024-02-23', '2424' ] );
-  await db.run("INSERT INTO Notes VALUES (?,?,?,?,?,?)", ['Mushrooms', 'Mushrooms are not nice!', '1', 'null', '2024-02-21', '15351' ] );
+  await db.run("INSERT INTO Notes VALUES (?,?,?,?,?,?)", ['Plants', 'Plants are nice!', 1, '', '2024-02-26', 12141 ] );
+  await db.run("INSERT INTO Notes VALUES (?,?,?,?,?,?)", ['Animals', 'Animals are nice!', 0, '', '2024-02-23', 2424 ] );
+  await db.run("INSERT INTO Notes VALUES (?,?,?,?,?,?)", ['Mushrooms', 'Mushrooms are not nice!', 1, 'null', '2024-02-21', 15351 ] );
 
 }
 
