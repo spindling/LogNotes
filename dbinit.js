e05c5982f94ebb8f7da93444eefe2eff8558c3c8
spindling <jeremy.sean009@gmail.com>
2026-02-28 20:07:06 -0500

    Implementing image load into dbinit. Currently returning undefined

--- dbinit.js
+++ dbinit.js
@@ -1,5 +1,6 @@
 const sqlite3 = require("sqlite3");
 const sqlite = require("sqlite");
+const fs = require("fs");
 
 async function dbinit()
 {
@@ -8,10 +9,13 @@ async function dbinit()
     driver: sqlite3.Database
   });
 
+  //read in images
+  const plantsImage = fs.readFile("/images/plants.jpg");
+
   await db.exec("DROP TABLE IF EXISTS Notes");
   await db.exec("CREATE TABLE Notes (title TEXT, content TEXT, starred INTEGER, image BLOB, timestamp TEXT, charcount INTEGER)");
 
-  await db.run("INSERT INTO Notes VALUES (?,?,?,?,?,?)", ['Plants', 'Plants are nice!', 1, '', '2024-02-26 02:02:02', 16 ] );
+  await db.run("INSERT INTO Notes VALUES (?,?,?,?,?,?)", ['Plants', 'Plants are nice!', 1, plantsImage, '2024-02-26 02:02:02', 16 ] );
   await db.run("INSERT INTO Notes VALUES (?,?,?,?,?,?)", ['Animals', 'Animals are nice!', 0, '', '2024-02-23 02:02:01', 17 ] );
   await db.run("INSERT INTO Notes VALUES (?,?,?,?,?,?)", ['Mushrooms', 'Mushrooms are not nice!', 1, '', '2024-02-21 02:02:00', 24 ] );
 
