72f9dc635fa7ee43a790e9a7f7ad799b58684482
spindling <jeremy.sean009@gmail.com>
2026-02-28 19:55:33 -0500

    Implement auto-timestamp. Update dbinit.js to include more realistic values

--- app.ctrl.js
+++ app.ctrl.js
@@ -40,7 +40,8 @@ app.post("/addnote", async function(req,res)
     const title = req.body.title;
     const content = req.body.content;
     const starred = (req.body.starred=="on") ? 1:0;
-    const timestamp = req.body.timestamp;
+    const date = new Date();
+    const timestamp = date.toLocaleDateString("sv") + " " + date.toLocaleTimeString("sv");
     const image = req.body.image;
     const charcount = req.body.charcount;
 
